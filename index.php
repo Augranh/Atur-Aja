67f6361b7a2bc790555f116bf4adef8fa5ff2732
cipilotdev <kuliah.informatika01@gmail.com>
2025-11-15 18:13:45 +0700

    fix vercel deploy

--- index.php
+++ index.php
@@ -3,6 +3,45 @@
     include('includes/navigation.php');
 ?>
 
+<?php
+    $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    switch ($request_uri) {
+        case '/':
+            require __DIR__ . '/pages/index.php';
+            break;
+
+        case '/login':
+            require __DIR__ . '/form/form-login.php';
+            break;
+
+        case '/register':
+            require __DIR__ . '/form/form-register.php';
+            break;
+
+        case '/dashboard':
+            require __DIR__ . '/pages/dashboard/index.php';
+            break;
+
+        case '/dashboard/tasks':
+            require __DIR__ . '/pages/dashboard/tasks.php';
+            break;
+        
+        case '/about':
+            require __DIR__ . '/pages/about.php';
+            break;
+
+        case '/register-handler':
+            require __DIR__ . '/register-handler.php';
+            break;
+        
+
+        default:
+            http_response_code(404);
+            echo '404 - Halaman tidak ditemukan';
+            break;
+    }
+?>
+
 <main>
     <?php
         include('includes/components/hero.php');
