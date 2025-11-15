f693ddd138db8effa9223c952330a674ffa96f02
cipilotdev <kuliah.informatika01@gmail.com>
2025-11-15 11:57:27 +0700

    M - fix final deployment error

--- api/index.php
+++ api/index.php
@@ -3,8 +3,8 @@
 // Redirect to main index.php
 require __DIR__ . '/../index.php';
 require __DIR__ . '/../includes/navigation.php';
-require __DIR__ . '/../includes/components/dashboard/sidebar.php';
-require __DIR__ . '/../includes/components/dashboard/header.php';
+// require __DIR__ . '/../includes/components/dashboard/sidebar.php';
+// require __DIR__ . '/../includes/components/dashboard/header.php';
 require __DIR__ . '/../includes/pages/dashboard/index.php';
 require __DIR__ . '/../includes/pages/auth/login.php';
 require __DIR__ . '/../includes/pages/auth/signup.php';
