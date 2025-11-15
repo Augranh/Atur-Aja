79b83abae258b8fc222e764510fda33f34daeea0
cipilotdev <kuliah.informatika01@gmail.com>
2025-11-15 12:15:02 +0700

    M - fix final deployment error

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 // Vercel serverless function entry point
 // Redirect to main index.php
-require __DIR__ . '/../index.php';
+require __DIR__ . '/../**';
 // require __DIR__ . '/../includes/navigation.php';
 // require __DIR__ . '/../includes/components/dashboard/sidebar.php';
 // require __DIR__ . '/../includes/components/dashboard/header.php';
